d3ed09c3bee938117675941f8c95d263865af897
YejinShin <devnana222@gmail.cim>
2024-10-01 12:28:50 +0900

    Init

---
+++ src/main/java/com/tdd/LectureRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureRegisterApplication.class, args);
+    }
+
+}
